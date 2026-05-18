34a98c672fd96770c30ae5fb46f08ec8ff3a8393
Jason Millard <jsm174@gmail.com>
2026-05-18 07:13:43 -0400

    deps: bump bgfx, bx, and bimg to latest. fix macos arm64 warning.

--- cmake/bx/bx.cmake
+++ cmake/bx/bx.cmake
@@ -100,10 +100,9 @@ target_compile_features(bx PUBLIC cxx_std_14)
 target_compile_options(bx PUBLIC $<$<CXX_COMPILER_ID:MSVC>:/Zc:__cplusplus /Zc:preprocessor>)
 
 # bx/include/bx/simd_t.h includes smmintrin.h unconditionally, so x86/x64 builds need SSE4.2.
-include(CheckCXXCompilerFlag)
-check_cxx_compiler_flag(-msse4.2 BX_HAS_MSSE42)
-if(BX_HAS_MSSE42)
-	target_compile_options(bx PUBLIC -msse4.2)
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "^(x86_64|amd64|AMD64|i[3-6]86|x86)$" OR
+	CMAKE_CXX_COMPILER_ARCHITECTURE_ID MATCHES "^(x86_64|amd64|AMD64|i[3-6]86|x86)$")
+	target_compile_options(bx PUBLIC $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-msse4.2>)
 endif()
 
 # Link against psapi on Windows
